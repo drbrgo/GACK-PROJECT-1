c7b6531e955645f5ef7c16d4fcad0db48a49a179
drbrgo <d.b.goga@gmail.com>
2023-12-09 16:09:28 -0500

    replaced all files

---
+++ src/main/java/org/launchcode/buildabeer/BuildABeerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.buildabeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildABeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildABeerApplication.class, args);
+	}
+
+}
