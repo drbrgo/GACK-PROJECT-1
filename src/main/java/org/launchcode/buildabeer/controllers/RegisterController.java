9e18f647d17604e6debd536503b32a9d4afe2aa4
MrsKing2023 <smarshall84@yahoo.com>
2023-12-16 17:10:38 -0500

    register page is accessible

--- src/main/java/org/launchcode/buildabeer/controllers/RegisterController.java
+++ src/main/java/org/launchcode/buildabeer/controllers/RegisterController.java
@@ -10,26 +10,20 @@ import java.util.List;
 
 @Controller
 @RequestMapping("register")
-public class UserController {
-    private static final List<String> users = new ArrayList<>();
+public class RegisterController {
+    private static final List<String> newUsers = new ArrayList<>();
     @GetMapping ("user") //this lives at /register/user
 
     public String registerHereForm() {
-        return "user";
+        return "register";
 }
-@PostMapping ("user")
+@PostMapping ("register")
 public String registerUser(@RequestParam String registerFirstName, String registerLastName, String birthdate, String phone, String email, String street, String city, String state, String zipCode) {
-        users.add(registerFirstName); users.add(registerLastName); users.add(birthdate); users.add(phone); users.add(email); users.add(street); users.add(city); users.add(state); users.add(zipCode);
-
-        //the if else statement is still not working properly
-        if (users.isEmpty()) {
-        System.out.println("All fields are required!");
-        return "redirect:";
-        } else {
+        newUsers.add(registerFirstName); newUsers.add(registerLastName); newUsers.add(birthdate); newUsers.add(phone); newUsers.add(email); newUsers.add(street); newUsers.add(city); newUsers.add(state); newUsers.add(zipCode);
         return "redirect:/submit"; //user will be redirected to submit confirmation page
 
     }
 
 }
 
-}
+
