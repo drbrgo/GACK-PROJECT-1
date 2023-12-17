f159f4bccd4a99c9e56f8feaf21951273ceb47ef
MrsKing2023 <smarshall84@yahoo.com>
2023-12-16 21:21:24 -0500

    register page is created not fully functional, however no errors. submit button needed once user registers and all fields need to be completed in order to register as user

--- src/main/java/org/launchcode/buildabeer/controllers/RegisterController.java
+++ src/main/java/org/launchcode/buildabeer/controllers/RegisterController.java
@@ -14,12 +14,14 @@ public class RegisterController {
     public String register() {
         return "register";
 }
+//TODO: user cannot leave fields blank
 @GetMapping ("register") //this lives at /register will take you directly to form
     public String displayRegisterForm(@RequestParam String registerFirstName, String registerLastName, String birthdate, String phone, String email, String street, String city, String state, String zipCode) {
         newUser.add(registerFirstName); newUser.add(registerLastName); newUser.add(birthdate); newUser.add(phone); newUser.add(email); newUser.add(street); newUser.add(city); newUser.add(state); newUser.add(zipCode);
         return "register";
     }
 
+    //TODO: information submitted must be stored and confirmation must be sent to user, page updates to reflect successfully registered.
     @PostMapping("register/add")
     public String addRegisterForm (@RequestParam String registerFirstName, String registerLastName, String birthdate, String phone, String email, String street, String city, String state, String zipCode) {
         newUser.add(registerFirstName); newUser.add(registerLastName); newUser.add(birthdate); newUser.add(phone); newUser.add(email); newUser.add(street); newUser.add(city); newUser.add(state); newUser.add(zipCode);
