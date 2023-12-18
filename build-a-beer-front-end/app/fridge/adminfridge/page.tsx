4aa4b07d23791dd6c4933abac7c96b5e808ae4c8
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2023-12-18 17:41:03 -0600

    Made FridgeAdmin component, but still ironing out how to connect to SQL to grab data.

---
+++ build-a-beer-front-end/app/fridge/adminfridge/page.tsx
@@ -0,0 +1,10 @@
+import FridgeAdmin from "@/components/fridge-admin/FridgeAdmin";
+
+export default function fridgeAdmin(){
+    return(
+    <div>
+    <p>This is the 'fridge admin' page</p>
+
+    <FridgeAdmin />
+    </div>)
+}
