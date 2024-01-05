22b9f3e8b80f25ed2718f8eb83c56e306cd7cbda
MrsKing2023 <smarshall84@yahoo.com>
2024-01-05 18:02:38 -0500

    updated components, put Main.js back and removed User.js. this will work better for team to follow along

--- build-a-beer/src/App.js
+++ build-a-beer/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import User from './components/User';
+import User from './components/Main';
 import Admin from './components/Admin';
+import Main from './components/Main';
 
 function App() {
   return (
     <div>
-      <User />
+      <Main />
       <Admin />
     </div>
   );
