91f2c2da5822c4d3a8c09a7637c881d29e596479
MrsKing2023 <138519556+MrsKing2023@users.noreply.github.com>
2024-01-08 21:18:18 -0500

    Revert "New user account"

--- src/main/java/org/launchcode/buildabeer/data/CreateProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.buildabeer.data;
-
-import org.launchcode.buildabeer.models.CreateProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreateProfileRepository extends JpaRepository<CreateProfile, Long> {
-}
