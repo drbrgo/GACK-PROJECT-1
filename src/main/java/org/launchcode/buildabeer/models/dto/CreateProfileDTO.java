1957d913b728948f1b19ba984bda30117dc94a14
MrsKing2023 <smarshall84@yahoo.com>
2024-01-09 22:52:19 -0500

    created new branch

---
+++ src/main/java/org/launchcode/buildabeer/models/dto/CreateProfileDTO.java
@@ -0,0 +1,85 @@
+package org.launchcode.buildabeer.models.dto;
+
+public class CreateProfileDTO {
+    private String firstName;
+    private String lastName;
+    private int birthdate;
+    private int phoneNumber;
+    private String emailAddress;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private int zipCode;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(int birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+}
