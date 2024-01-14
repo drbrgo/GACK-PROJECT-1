349716115b3a450f0051dfde585e64c3aee1709d
drbrgo <d.b.goga@gmail.com>
2024-01-14 18:59:03 -0500

    working through punkapi integration. committing before installing jackson dependencies for json object building (so that i can return a list of beer names)

--- src/front_end/app/components/authentication/LoginForm.js
+++ src/front_end/app/components/authentication/LoginForm.js
@@ -2,69 +2,6 @@
 // import React, { useState } from 'react';
 
 
-// export default function LoginForm() {
-    
-//     const webUrl = "http://localhost:8080/"
-
-//     // manage input values
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   // manage error message
-//   const [errorMessage, setErrorMessage] = useState('');
-
-
-//     const handleSubmit = async(event) => {
-//         event.preventDefault();
-
-//         const data = {
-//             username: event.target.username.value,
-//             password: event.target.password.value
-//         }
-
-//         //console.log(data);
-//         await fetch(webUrl, {
-//             method: 'POST',
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify(data),
-//         }).then((response) => response.json()).then(data => {
-//             console.log(data);
-//             //this sends the data to the backend, and then back to the front end to test that it works. 
-//         })
-//     }
-
-//     return (
-//         <div className= "flex justify-center">
-//             <form onSubmit = {handleSubmit}>
-//                 <h1>Login</h1>
-//                 <div class="container">
-//                 <div class="row">
-//                     <div class="form-item col-4">
-//                         <label>Username</label>
-//                         <input type="text" autoComplete="off" id="username" />
-//                     </div>
-//                 </div>
-//                 </div>
-//                 <div class="container">
-//                 <div class="row">
-//                     <div class="form-item col-4">
-//                         <label>Password</label>
-//                         <input type="password" autoComplete="off" id="password" />
-//                     </div>
-//                 </div>
-//          </div>
-
-//                 <button type="submit">Login!</button>
-//             </form>
-//         </div>
-//     )
-
-    
-
-
-// }
-
 import { setCookie } from 'cookies-next';
 import { parseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 import { getCookies } from 'cookies-next';
--- src/front_end/app/user/beercreated/page.js
+++ src/front_end/app/user/beercreated/page.js
@@ -1,9 +1,15 @@
+'use client';
+
 import React from 'react';
+import BeerNameGenerator from '../../components/beer-name-generator/BeerNameGenerator';
+import BeerNameGenerator2 from '../../components/beer-name-generator/BeerNameGenerator2';
+//import { useState, useEffect } from "react";
 
 const BeerCreated = () => {
     return(
         <div>
             <p>This is the beer is created page!</p>
+            <BeerNameGenerator />
         </div>
     )
 }
