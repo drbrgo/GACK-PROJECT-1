e876f7834aab84a60269e738bfad46c5d3eeb043
real-folkblues <marenicolor@gmail.com>
2024-01-13 22:06:52 -0500

    accidentally duplicated main folder outside of src, undid action

--- main/java/org/launchcode/buildabeer/data/RegisterDummyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.buildabeer.data;
-
-import org.launchcode.buildabeer.models.RegisterDummy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterDummyRepository extends JpaRepository<RegisterDummy, Integer> {
-}
