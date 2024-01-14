02e60f193d75d501a5e5c9106e8f6ca265f22872
real-folkblues <marenicolor@gmail.com>
2024-01-14 16:24:37 -0500

    form front end/CRUD reached MVP, need to add validation

--- src/front_end/app/user/createbeer/brewForm.js
+++ src/front_end/app/user/createbeer/brewForm.js
@@ -11,23 +11,23 @@ export default function CreateBeer(props: any) {
             console.log("Submitted")
             }
 
-        const data = {
+        const featureData = {
             username: String(event.target.username.value),
             Wolfenstein: String(event.target.Wolfenstein.value),
             barrelType: String(event.target.barrelType.value),
-            basic: String(event.target.basic.value),
+            basic: IsNum(event.target.basic.value),
             recentPlace: String(event.target.recentPlace.value),
             userComments: String(event.target.userComments.value),
         }
 
-        await fetch(webUrl + "/beercreated", { //beercreated??
+        await fetch(webUrl + "user/createbeer", { //??
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(data),
-        }).then((response) => response.json()).then((data: BeerCreated[]) => {
-            props.setBeerCreated(data);
+        }).then((response) => response.json()).then((featureData: BeerCreated[]) => {
+            props.setBeerCreated(featureData);
 
             event.target.username.value = ""
             event.target.Wolfenstein.value = ""
@@ -45,61 +45,105 @@ export default function CreateBeer(props: any) {
         <div>
 <link rel="stylesheet" href="formStyle.css" />
 <form id="brewForm" onSubmit={formSubmit}> //name of form is createbeer?
-//        <!--<script src="" href=""></script>-->
-        <h1>Create your beer!</h1>
-            <fieldset>
-                        //<h1> username </h1>
-                        //will be auto-populated assuming user is logged in. will add html for this later
-
-                        <h1>What are your flavor preferences?</h1>
-
-                           <input type="checkbox" name="extras" value="fruity"> Fruity </>
-                           <input type="checkbox" name="extras" value="malty"> Malty </>
-                           <input type="checkbox" name="extras" value="sour"> Sour </>
-                           <input type="checkbox" name="extras" value="sweet"> Sweet </>
-                           <input type="checkbox" name="extras" value="bitter"> Bitter </>
-                           <input type="checkbox" name="extras" value="hoppy"> Hoppy </>
-                           <input type="checkbox" name="extras" value="floral"> Floral </>
-                           <input type="button" value="Submit" onclick="getSelectedCheckboxes()">
+//        <<h1>Proprietary Brew Form</h1>
+          </head>
+              <body>
 
 
-                               In what type of barrels do you like your brewski to be made?
-                               <input type="radio" name="frenchOak" required value="French Oak">  </></label></p>
-                               <input type="radio" name="AmericanOak" required value="American Oak">  </></label></p>
-                               <input type="radio" name="Cedar" required value="Cedar">  </></label></p>
+          <h1>Proprietary Brew Form</h1>
+          <br>
+          <label for="username">Welcome!</label>
+          <input type="text" id="username" name="username" value="username from somewhere" readonly><br>
+          <!--//reminder to grab cookie? from drew-->
+          <br>
+          <br>
+              <fieldset>
+                  <legend>How strong of a beer are ya lookin' for today?</legend>
+                  <div>
+                  <input type="radio" name="Wolfenstein" required value="easy"><label>Can I play, Daddy?</label>
+                  </div>
+                  <div>
+                  <input type="radio" name="Wolfenstein" required value="medium"><label>Bring 'em on!</label>
+                  </div>
+                  <div>
+                  <input type="radio" name="Wolfenstein" required value="hard"><label>I am Death incarnate!</label>
+                  </div>
+               </fieldset>
+          <br>
+              <fieldset>
+                  <legend>Preferred flavor notes</legend>
+                  <div>
+                   <input type="checkbox" id="fruity" name="extras" value="fruity" onclick="getSelectedCheckboxes()"><label for="fruity">Fruity</label>
+                   </div>
+                     <div>
+                   <input type="checkbox" id="malty" name="extras" value="malty" onclick="getSelectedCheckboxes()"><label for="malty">Malty</label>
+                     </div>
+                  <div>
+                   <input type="checkbox" id="sour"  name="extras" value="sour" onclick="getSelectedCheckboxes()"><label for="sour">Sour</label>
+                  </div>
+                  <div>
+                   <input type="checkbox" id="sweet" name="extras" value="sweet" onclick="getSelectedCheckboxes()"><label for="sweet">Sweet</label>
+                  </div>
+                  <div>
+                   <input type="checkbox" id="bitter" name="extras" value="bitter" onclick="getSelectedCheckboxes()"><label for="bitter">Bitter</label>
+                  </div>
+                  <div>
+                   <input type="checkbox" id="hoppy" name="extras" value="hoppy" onclick="getSelectedCheckboxes()"><label for="hoppy">Hoppy</label>
+                  </div>
+                  <div>
+                  <label> <input type="checkbox" name="extras" value="floral" onclick="getSelectedCheckboxes()">Floral</label>
+                  </div>
+                  </fieldset>
+          <br>
 
-                           <p>
-                               <label>
-                                   <select id="barrelType" name="barrelType">
-                                       <option value="" selected="selected">Select One</option>
-                                       <option value="frenchOak">French Oak</option>
-                                       <option value="americanOak">American Oak</option>
-                                       <option value="cedar">Cedar</option>
-                                   </select>
-                               </label>
-                           </p>
+                   <fieldset>
+                       <legend>Barrel Type</legend>
+                   <select id="barrelType" name="barrelType">
+                       <option value="" name="selected">Select One</option>
+                       <option value="frenchOak">French Oak</option>
+                       <option value="americanOak">American Oak</option>
+                       <option value="cedar">Cedar</option>
+                   </select></fieldset>
 
-                           <p>
-                               <label>Where was the last place you had a beer?
-                                   <input type="text" name="recentPlace" required list="locations">
-                               </></label>
 
-                               <datalist id="locations">
-                                   <option value="At home">
-                                   <option value="The beach">
-                                   <option value="In a cave">
-                               </datalist>
-                           </p>
+          <br>
+          <br>
+          <fieldset>
+              <legend>Which one of these is the Pumpkin Spice Latte of beers?</legend><br>
 
-                           <p>
-                               <label>If animals could talk, which would be the rudest? Enter below
-                                   <textarea name="comments" maxlength="500"></textarea>
-                               </label>
-                           </p>
+              <label for="Blue Moon">Blue Moon</label>
+              <input type="color" id="3" name="favcolor" value="#dc7633">
+              <label for="Budwieser">Budweiser</label>
+              <input type="color" id="2" name="favcolor" value="#f8c471">
+              <label for="Samuel Adams">Samuel Adams</label>
+              <input type="color" id="3" name="favcolor" type="color" value="#b03a2e">
+              </fieldset>
+          <br>
+                 <br>
 
-                           <p><div class="animatedBox"><button id="loading-button">Build my Beer!</button>
-                           <script src="buttonloader.js"></script></div></p>
 
+
+                   <legend>Where was the last place you had a beer?</legend>
+                   <br>
+                  <input type="text" name="recentPlace" required list="locations">
+
+                  <datalist id="locations">
+                      <option value="At home">
+                      <option value="The beach">
+                      <option value="In a cave">
+                  </datalist>
+
+          <br><br><br>
+          <legend>If animals could talk, which one would be the most rude? Enter below<legend>
+              <br>
+                  <label for="comments"></label>
+          <div class="commentBox"><textarea id="comments" name="userComments" rows="4"></textarea>
+          <br>
+          //add validation for textbox
+
+          </body>
+
+          <div class="animatedBox"><button id="loading-button">Build my Beer!</button>
             )
         }
 
