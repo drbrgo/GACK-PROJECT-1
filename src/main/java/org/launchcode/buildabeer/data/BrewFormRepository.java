8b36850217f02074db134f445d57679231f7098a
real-folkblues <marenicolor@gmail.com>
2024-01-17 01:16:39 -0500

    removed files originating from marleny

--- src/main/java/org/launchcode/buildabeer/data/BrewFormRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.buildabeer.data;
-
-import org.launchcode.buildabeer.models.BrewForm;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BrewFormRepository extends CrudRepository<BrewForm, Integer> {
-}
--- src/main/java/org/launchcode/buildabeer/models/BrewFormInterface.java
+++
@@ -1,8 +0,0 @@
-package org.launchcode.buildabeer.models;
-
-public interface BrewFormInterface {
-    //unsure whether getter and setters are needed here...i'll look into it
-    String getLoginName();
-
-    void setLoginName(String username);
-}
