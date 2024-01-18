98b9bd3ecb0a2c97c5b49ca3d68bd2a1023f61fb
drbrgo <d.b.goga@gmail.com>
2024-01-17 22:45:19 -0500

    authorization work in CheckForLoggedIn component

--- src/front_end/app/user/profiledummy/page.js
+++ src/front_end/app/user/profiledummy/page.js
@@ -3,10 +3,12 @@
 import NavBarDummy from '../../components/authentication/NavBarDummy';
 import React from 'react';
 import LogoutForm from '../../components/authentication/LogoutForm';
+import CheckForLoggedIn from '../../components/authentication/CheckForLoggedIn';
 
 const ProfilePageDummy = () => {
     return(
         <div>
+            <CheckForLoggedIn />
             <p>This is the user profile page!</p>
             {/* <NavBarDummy /> we'll see if it works without it and the answer is still no*/}
             {/* <LogoutForm /> has moved to the user/logout page*/}
